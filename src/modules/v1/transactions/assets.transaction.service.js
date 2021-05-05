e8751bd0fe9e4bd36e83d7c2818b3806244a1395
Sterling Wells <s.wells@topl.me>
2021-05-05 16:20:11 -0400

    Committing so I can apply stash

--- src/modules/v1/transactions/assets.transaction.service.js
+++ src/modules/v1/transactions/assets.transaction.service.js
@@ -9,18 +9,6 @@ const serviceName = "AssetTransaction";
 
 class AssetTransactionService {
   static async createAsset(args) {
-<<<<<<< HEAD
-    const bramblHelper = new BramblHelper(
-      false,
-      args.password,
-      args.network,
-      args.keyFilePath
-    );
-    if (bramblHelper) {
-      return await RequestValidator.validateBody(args, "create").then(function(
-        result
-      ) {
-=======
     return await RequestValidator.validateBody(args).then(function(result) {
       const bramblHelper = new BramblHelper(
         false,
@@ -29,7 +17,6 @@ class AssetTransactionService {
         args.keyFilePath
       );
       if (bramblHelper) {
->>>>>>> 4f842a6c0a6336f9ac07cce6f3a7668047dba472
         var assetCode = bramblHelper.createAssetValue(args.name);
         result.assetCode = assetCode;
         result.name = args.name;
@@ -74,51 +61,6 @@ class AssetTransactionService {
   }
 
   static async updateAsset(args) {
-<<<<<<< HEAD
-    const bramblHelper = new BramblHelper(
-      false,
-      args.password,
-      args.network,
-      args.keyFilePath
-    );
-    if (bramblHelper) {
-      return await RequestValidator.validateBody(args, "transfer").then(
-        function(result) {
-          if (result.assetCode != null) {
-            result.minting = false;
-            return bramblHelper.assetTransaction(result);
-          } else {
-            throw stdError(
-              404,
-              "Missing or Invalid Asset Code",
-              serviceName,
-              serviceName
-            );
-          }
-        }
-      );
-    } else {
-      throw stdError(
-        400,
-        "Unable to create transaction, please double check your request.",
-        serviceName,
-        serviceName
-      );
-    }
-  }
-
-  static async burnAsset(args) {
-    const bramblHelper = new BramblHelper(
-      false,
-      args.password,
-      args.network,
-      args.keyFilePath
-    );
-    if (bramblHelper) {
-      return await RequestValidator.validateBody(args, "burn").then(function(
-        result
-      ) {
-=======
     return await RequestValidator.validateBody(args).then(function(result) {
       const bramblHelper = new BramblHelper(
         false,
@@ -127,7 +69,6 @@ class AssetTransactionService {
         args.keyFilePath
       );
       if (bramblHelper) {
->>>>>>> 4f842a6c0a6336f9ac07cce6f3a7668047dba472
         if (result.assetCode != null) {
           const concatEvidence = Buffer.from(Constants.CONCAT_EVIDENCE);
           const hashChecksumBuffer = blake
@@ -195,6 +136,73 @@ class AssetTransactionService {
       }
     });
   }
+
+  static async burnAsset(args) {
+    return await RequestValidator.validateBody(args).then(function(result) {
+      const bramblHelper = new BramblHelper(
+        false,
+        args.password,
+        args.network,
+        args.keyFilePath
+      );
+      if (bramblHelper) {
+        if (result.assetCode != null) {
+          result.recipients = [[Base58.encode(address), args.quantity]];
+          result.minting = false;
+          return bramblHelper
+            .sendRawAssetTransaction(result)
+            .then(function(value) {
+              if (value.error != null) {
+                throw stdError(500, value.error, serviceName, serviceName);
+              } else {
+                return bramblHelper
+                  .signTransaction(value)
+                  .then(function(value) {
+                    if (value.error != null) {
+                      throw stdError(
+                        500,
+                        value.error,
+                        serviceName,
+                        serviceName
+                      );
+                    } else {
+                      return bramblHelper
+                        .sendSignedTransaction(value)
+                        .then(function(value) {
+                          if (value.error != null) {
+                            throw stdError(
+                              500,
+                              value.error,
+                              serviceName,
+                              serviceName
+                            );
+                          } else {
+                            return value;
+                          }
+                        });
+                    }
+                  });
+              }
+            });
+        } else {
+          throw stdError(
+            404,
+            "Missing or Invalid Asset Code",
+            serviceName,
+            serviceName
+          );
+        }
+      } else {
+        throw stdError(
+          400,
+          "Unable to create transaction, please double check your request.",
+          serviceName,
+          serviceName
+        );
+      }
+    });
+  }
+}
 }
 
 module.exports = AssetTransactionService;
