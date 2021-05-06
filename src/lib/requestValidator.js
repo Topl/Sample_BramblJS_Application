69d4376cc252507debcd8a319559ae3f058f0f26
Sterling Wells <s.wells@topl.me>
2021-05-06 16:03:33 -0400

    Removed service validation of request

--- src/lib/requestValidator.js
+++ src/lib/requestValidator.js
@@ -1,7 +1,4 @@
 const BramblJS = require("brambljs");
-const { MAX_INTEGER, MAX_METADATA_LENGTH } = require("../util/constants");
-
-const PROPOSITION_TYPES = ["PublicKeyCurve25519", "ThresholdCurve25519"];
 
 class RequestValidator {
   static validateAddresses(value, network) {
@@ -30,91 +27,6 @@ class RequestValidator {
       return Promise.reject("Please provide a valid network");
     }
     return Promise.resolve();
-  }
-
-  static async validateBody(body) {
-    return new Promise((resolve, reject) => {
-      if (typeof body === "object" && Object.keys(body).length != 0) {
-        let obj = {};
-        obj.propositionType = PROPOSITION_TYPES.find(proposition => {
-          return proposition === body.propositionType;
-        })
-          ? body.propositionType
-          : null;
-        obj.network = body.network;
-        const isNetworkValid = RequestValidator.validateNetwork(obj.network);
-        if (!isNetworkValid) {
-          obj.error = "Network Invalid";
-        }
-        const senderAddresses = body.senders.map(x => x[0]);
-        if (
-          !RequestValidator.validateAddresses(senderAddresses, body.network)
-        ) {
-          obj.error = "Invalid sender address detected";
-        }
-        obj.senders = body.senders;
-
-        const recipientAddresses = body.recipients.map(x => x[0]);
-        if (
-          !RequestValidator.validateAddresses(recipientAddresses, body.network)
-        ) {
-          obj.error = "Invalid recipient address detected";
-        }
-
-        if (Array.isArray(body.recipients)) {
-          for (var i = 0; i < body.recipients.length; i++) {
-            if (
-              body.recipients[i][1] < 0 ||
-              body.recipients[i][1] > MAX_INTEGER
-            ) {
-              obj.error = "invalid quantity";
-            }
-          }
-          obj.recipients = body.recipients;
-        } else {
-          obj.error = "recipients is not an array of [String, String]";
-        }
-        obj.changeAddress = body.changeAddress;
-        const changeAddressValid = RequestValidator.validateAddresses(
-          [body.changeAddress],
-          body.network
-        );
-        if (!changeAddressValid) {
-          obj.error = "Change Address Invalid";
-        }
-        obj.consolidationAddress = body.consolidationAddress;
-        if (obj.consolidationAddress != null) {
-          if (
-            !RequestValidator.validateAddresses(
-              [obj.consolidationAddress],
-              body.network
-            )
-          ) {
-            obj.error = "Consolidation Address Invalid";
-          }
-        }
-        obj.assetCode = body.assetCode;
-        if (obj.assetCode != null) {
-          if (!RequestValidator.validateAsset(obj.assetCode)) {
-            obj.error = "Asset Code Invalid";
-          }
-        }
-        obj.metadata = body.metadata;
-        if (obj.metadata != null) {
-          if (obj.metadata > MAX_METADATA_LENGTH) {
-            obj.error =
-              "Attached data is greater than the maximum character length. Please include shorter data";
-          }
-        }
-        if (obj.error) {
-          reject(new Error(obj.error));
-        } else {
-          resolve(obj);
-        }
-      } else {
-        reject(new Error("Missing or invalid request json object"));
-      }
-    });
   }
 }
 
--- src/modules/v1/transactions/assets.transaction.service.js
+++ src/modules/v1/transactions/assets.transaction.service.js
@@ -1,80 +1,69 @@
 const BramblHelper = require("../../../lib/bramblHelper");
-const RequestValidator = require("../../../lib/requestValidator");
 const stdError = require("../../../core/standardError");
 
 const serviceName = "AssetTransaction";
 
 class AssetTransactionService {
   static async createAsset(args) {
-    return await RequestValidator.validateBody(args).then(function(result) {
-      const bramblHelper = new BramblHelper(
-        false,
-        args.password,
-        args.network,
-        args.keyFilePath
+    const bramblHelper = new BramblHelper(
+      false,
+      args.password,
+      args.network,
+      args.keyFilePath
+    );
+    if (bramblHelper) {
+      var assetCode = bramblHelper.createAssetValue(args.name);
+      args.assetCode = assetCode;
+      return bramblHelper.sendRawAssetTransaction(args).then(function(value) {
+        if (value.error) {
+          throw stdError(500, value.error, serviceName, serviceName);
+        } else {
+          return bramblHelper.signAndSendTransaction(value);
+        }
+      });
+    } else {
+      throw stdError(
+        400,
+        "Unable to create transaction, please double check your request.",
+        serviceName,
+        serviceName
       );
-      if (bramblHelper) {
-        var assetCode = bramblHelper.createAssetValue(args.name);
-        result.assetCode = assetCode;
-        result.name = args.name;
-        result.minting = args.minting;
-        return bramblHelper
-          .sendRawAssetTransaction(result)
-          .then(function(value) {
-            if (value.error) {
-              throw stdError(500, value.error, serviceName, serviceName);
-            } else {
-              return bramblHelper.signAndSendTransaction(value);
-            }
-          });
-      } else {
-        throw stdError(
-          400,
-          "Unable to create transaction, please double check your request.",
-          serviceName,
-          serviceName
-        );
-      }
-    });
+    }
   }
 
   static async updateAsset(args) {
-    return await RequestValidator.validateBody(args).then(function(result) {
-      const bramblHelper = new BramblHelper(
-        false,
-        args.password,
-        args.network,
-        args.keyFilePath
-      );
-      if (bramblHelper) {
-        if (result.assetCode) {
-          result.minting = false;
-          return bramblHelper
-            .sendRawAssetTransaction(result)
-            .then(function(value) {
-              if (value.error) {
-                throw stdError(500, value.error, serviceName, serviceName);
-              } else {
-                return bramblHelper.signAndSendTransaction(value);
-              }
-            });
-        } else {
-          throw stdError(
-            404,
-            "Missing or Invalid Asset Code",
-            serviceName,
-            serviceName
-          );
-        }
+    const bramblHelper = new BramblHelper(
+      false,
+      args.password,
+      args.network,
+      args.keyFilePath
+    );
+    if (bramblHelper) {
+      if (args.assetCode) {
+        args.minting = false;
+        return bramblHelper.sendRawAssetTransaction(args).then(function(value) {
+          if (value.error) {
+            throw stdError(500, value.error, serviceName, serviceName);
+          } else {
+            return bramblHelper.signAndSendTransaction(value);
+          }
+        });
       } else {
         throw stdError(
-          400,
-          "Unable to create transaction, please double check your request.",
+          404,
+          "Missing or Invalid Asset Code",
           serviceName,
           serviceName
         );
       }
-    });
+    } else {
+      throw stdError(
+        400,
+        "Unable to create transaction, please double check your request.",
+        serviceName,
+        serviceName
+      );
+    }
   }
 }
 
